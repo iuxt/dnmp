7bd1813e6c5b24a7d4a716bdb117fcc496b01ee5
zhanglikun <iuxt@qq.com>
2021-08-26 14:42:49 +0800

    expose mysql port on shell script

--- start-mysql-amd64.sh
+++ start-mysql-amd64.sh
@@ -3,5 +3,6 @@ docker run --name=mysql \
 	-v $(pwd)/mysql_data:/var/lib/mysql \
 	-e MYSQL_ROOT_PASSWORD=123456 \
 	-e MYSQL_DATABASE=wordpress \
+        -p 3306:3306 \
 	--restart=always \
 	-d mysql:5.7.32
--- start-mysql-arm.sh
+++ start-mysql-arm.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 docker run --name=mysql \
     -v $(pwd)/mysql_data:/var/lib/mysql \
+    -p 3306:3306 \
     -d mysql/mysql-server
     
 docker logs mysql 2>&1 | grep GENERATED
