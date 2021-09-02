f53c722f51ffb8a25e5528cc1158eef6e024e468
zhanglikun <iuxt@qq.com>
2021-09-02 10:08:08 +0800

    add restart always

--- start-mysql-arm.sh
+++ start-mysql-arm.sh
@@ -2,6 +2,8 @@
 docker run --name=mysql \
     -v $(pwd)/mysql_data:/var/lib/mysql \
     -p 3306:3306 \
+    --restart=always \
     -d mysql/mysql-server
     
+sleep 10
 docker logs mysql 2>&1 | grep GENERATED
