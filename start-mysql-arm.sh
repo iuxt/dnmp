41bf51831f53519014382eea8cc4d2fa74cc4c8c
zhanglikun <iuxt@qq.com>
2021-09-02 10:09:42 +0800

    move mysql scripts to linux scripts

--- start-mysql-arm.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-docker run --name=mysql \
-    -v $(pwd)/mysql_data:/var/lib/mysql \
-    -p 3306:3306 \
-    --restart=always \
-    -d mysql/mysql-server
-    
-sleep 10
-docker logs mysql 2>&1 | grep GENERATED
