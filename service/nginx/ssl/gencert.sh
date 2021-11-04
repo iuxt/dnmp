0e8649cfd4fd7bb77972942cb989fd6d8470068e
zhanglikun <iuxt@qq.com>
2021-11-04 17:09:12 +0800

    add OU

--- service/nginx/ssl/gencert.sh
+++ service/nginx/ssl/gencert.sh
@@ -6,7 +6,7 @@ if [ -e ca.crt ]; then
     echo ca exists 
 else
     openssl ecparam -out ca.key -name prime256v1 -genkey
-    openssl req -new -sha256 -key ca.key -out ca.csr -subj "/C=CN/ST=ShangHai/L=PuDong/O=Bimawen/CN=bimawen.cc"
+    openssl req -new -sha256 -key ca.key -out ca.csr -subj "/C=CN/ST=ShangHai/L=PuDong/O=Bimawen/OU=Bimawen/CN=bimawen.cc"
     openssl x509 -req -sha256 -days 36500 -in ca.csr -signkey ca.key -out ca.crt 
 fi
 
@@ -14,6 +14,6 @@ fi
 ## 创建服务器证书
 echo "========================================================================================================================================"
 openssl ecparam -out server.key -name prime256v1 -genkey
-openssl req -new -sha256 -key server.key -out server.csr -subj "/C=CN/ST=ShangHai/L=PuDong/O=zhanglikun/CN=test.com"
+openssl req -new -sha256 -key server.key -out server.csr -subj "/C=CN/ST=ShangHai/L=PuDong/O=zhanglikun/OU=test.com/CN=test.com"
 openssl x509 -req -in server.csr -CA  ca.crt -CAkey ca.key -CAcreateserial -out server.crt -days 365 -sha256
 openssl x509 -in server.crt -text -noout
